75103926ce426fa728b9e1e81349a824e47ccdc1
Rick Tankard <trickytank@users.noreply.github.com>
2018-03-22 16:29:39 +0800

    Update run_strexpansion_score.sh
    Making the example script more clear

--- examples/run_strexpansion_score.sh
+++ examples/run_strexpansion_score.sh
@@ -2,27 +2,31 @@
 
 ### Configuration
 
-# Set bam_glob to include all bam files you wish to analyse (multiple runs may be combined in R exSTRa later)
+# Set bam_glob to a glob pattern that includes all bam files you wish to analyse.
+# (multiple runs may be combined in R exSTRa later)
+# You may separate globs by spaces or give full paths within the quotes.
+# (For more complex cases, set in the perl command directly)
 bam_glob="path/to/bams/*.bam"
 
-# Output tab delimited file
+# Output tab delimited file path:
 output=output/exSTRa_scores.txt
 
-# As provided from the R exSTRa package (https://github.com/bahlolab/exSTRa)
-# Check that the reference matches your BAM files
+# As provided from the R exSTRa package (https://github.com/bahlolab/exSTRa).
+# Check that the reference matches your BAM files.
 repeat_database=path/to/repeat_expansion_disorders.txt
 
-# Reference FASTA file, the same as BAM files
+# Reference FASTA file, the same as BAM files:
 reference=path/to/hg19.fa
 
+# exSTRa_score.pl path
+exstra_score="bin/exSTRa_score.pl"
+
 ### Running ###
-# Create output
 mkdir -p $(dirname $output)
 
-# Set the correct path for the exSTRa_score.pl script
-perl bin/exSTRa_score.pl \
+# Please set the correct path for the exSTRa_score.pl script
+perl "$exstra_score" \
     "$reference" \
     "$repeat_database" \
     $bam_glob \
     > "$output"
-
