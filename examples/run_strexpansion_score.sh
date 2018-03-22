efc3b4b45888b7cb79314b782005494a8716a736
Rick Tankard <tankard@wehi.edu.au>
2018-03-22 19:19:22 +1100

    Updated README and made example script clearer

--- examples/run_strexpansion_score.sh
+++ examples/run_strexpansion_score.sh
@@ -1,14 +1,26 @@
 # Running exSTRa
 
+### Configuration
+
+# Set bam_glob to include all bam files you wish to analyse (multiple runs may be combined in R exSTRa later)
 bam_glob="path/to/bams/*.bam"
+
+# Output tab delimited file
 output=output/exSTRa_scores.txt
 
-repeat_database=path/to/repeat_disorders.xlsx
+# As provided from the R exSTRa package (https://github.com/bahlolab/exSTRa)
+# Check that the reference matches your BAM files
+repeat_database=path/to/repeat_expansion_disorders.txt
+
+# Reference FASTA file, the same as BAM files
 reference=path/to/hg19.fa
 
+### Running ###
+# Create output
 mkdir -p $(dirname $output)
 
-perl exSTRa_score.pl \
+# Set the correct path for the exSTRa_score.pl script
+perl bin/exSTRa_score.pl \
     "$reference" \
     "$repeat_database" \
     $bam_glob \
